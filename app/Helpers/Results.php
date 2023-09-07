190935fa30d50600d2396d45621dc4cc04ed1bcd
carloeusebi <carloeusebi@gmail.com>
2023-09-07 12:23:35 +0200

    Improved comments in helper Results

--- app/Helpers/Results.php
+++ app/Helpers/Results.php
@@ -10,11 +10,13 @@ use Exception;
 class Results
 {
 
+  private Survey $survey;
   private Patient $patient;
   private Question $activeQuestion;
 
-  public function __construct(private Survey $survey)
+  public function __construct(Survey $survey)
   {
+    $this->survey = $survey;
     $this->patient = $survey->patient;
     return $this;
   }
@@ -45,8 +47,7 @@ class Results
     $this->activeQuestion = $question;
 
     foreach ($variables as &$variable) {
-      $score = $this->calculateVariableScore($variable);
-      $variable['score'] = $score;
+      $variable['score'] = $score = $this->calculateVariableScore($variable);
       foreach ($variable['cutoffs'] as &$cutoff) {
         $cutoff['scored'] = $this->hasScored($score, $variable, $cutoff);
       }
@@ -61,8 +62,9 @@ class Results
   private function calculateVariableScore(array $variable): int
   {
     $score = 0;
+    [$min, $max] = $this->activeQuestion->getBoundaries();
     foreach ($variable['items'] as $itemId) {
-      $score += $this->getItemScore($itemId);
+      $score += $this->getItemScore($min, $max, $itemId);
     }
     return $score;
   }
@@ -76,10 +78,9 @@ class Results
    * @return int The item's answer score.
    * @throws Exception If the item's answer is not found.
    */
-  private function getItemScore(string $id)
+  private function getItemScore(int $min, int $max, string $id)
   {
     $question = $this->activeQuestion;
-    [$min, $max] = $this->activeQuestion->getBoundaries();
 
     $index = array_search($id, array_column($question->items, 'id'));
     $item = $question->items[$index];
@@ -88,14 +89,18 @@ class Results
     // if answer is -1 it means the answer was skipped
     if ($answer === -1) return 0;
 
+    // throws an error if answer is outside min and max boundaries.
     if ($answer < $min || $answer > $max)
       throw new Exception("La risposta dell'item \"{$item['id']}.{$item['text']}\" del questionario {$question->question} contiene un punteggio non valido: $answer");
 
+    // if item has reversed score reverse the score
     if (isset($item['reversed']) && $item['reversed'])
       $answer = $this->reverseScore($min, $max, $answer);
 
+    // if question type is edi (0,0,0,1,2,3) it needs to calculate the score for the answer
     if ($question->type === 'EDI')
       $answer = $answer - 2 < 0 ? 0 : $answer - 2;
+    // if question type is mul the answer score taken by the item multipleAnswers->singleAnswer->points  attribute
     elseif ($question->type === 'MUL')
       $answer = $item['multipleAnswers'][$answer]['points'];
 
@@ -112,6 +117,9 @@ class Results
   }
 
 
+  /**
+   * Checks if the score is outside the cutoff's limits.
+   */
   private function hasScored(int $score, array $variable, array $cutoff): bool
   {
     $patient = $this->patient;
@@ -119,10 +127,13 @@ class Results
     $from = $cutoff['from'];
     $to = $cutoff['to'];
 
+    //Some cutoffs are gender based, for example male has a cutoff of 6 and female of 9
     if (isset($variable['genderBased']) && $variable['genderBased']) {
+      // if the cutoff is gender based and the patient doesn't have a sex assigned throws an error.
       if (!isset($patient->sex) || !in_array($patient->sex, ['M', 'F', 'O']))
         throw new Exception("Uno dei questionari ha cutoffs basati sul sesso ma {$patient->fname} {$patient->lname} non ha nessun sesso assegnato.");
 
+      // if cutoff is gender based assign to form and to the values for female.
       if ($patient->sex === 'F') {
         $from = $cutoff['femFrom'] ?? null;
         $to = $cutoff['femTo'] ?? null;
@@ -131,6 +142,7 @@ class Results
 
     $type = $cutoff['type'];
 
+    // returns if the score is outside the cutoff's limit
     switch ($type) {
       case 'greater-than':
         return $score > $from;
